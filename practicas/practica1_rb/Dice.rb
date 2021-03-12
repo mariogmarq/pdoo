01d87a6c78c48f80af1b66c0b65d981d94c2ac8e
mariogmarq <mariogarciamarq@gmail.com>
2021-03-12 10:32:50 +0100

    Main Ruby acabado

--- practicas/practica1_rb/Dice.rb
+++ practicas/practica1_rb/Dice.rb
@@ -18,10 +18,10 @@ module Deepspace
 
     #Método que determina el número de hangares que recibirá una estación espacial al ser creada
     def initWithNHangars
-      if @generator.rand 1.0 < @NHANGARSPROB
-        return 0
+      if @generator.rand(1.0) < @NHANGARSPROB
+        0
       else 
-        return 1
+        1
       end
     end
 
@@ -29,20 +29,20 @@ module Deepspace
     def initWithNWeapons
       guess = @generator.rand(1.0)
       if guess < @NWEAPONSPROB
-        return 1
+        1
       elsif guess < 2 * @NWEAPONSPROB
-        return 2
+        2
       else
-        return 3
+        3
       end
     end
 
     #Método que determina el número de potenciadores de escudo que recibirá una estación espacial al ser creada
     def initWithNShields
       if @generator.rand(1.0) < @NSHIELDSPROB
-        return 0
+        0
       else
-        return 1
+        1
       end
     end
 
@@ -54,15 +54,15 @@ module Deepspace
     #Método que determina quién dispara primero en un combate: la estación espacial o la nave enemiga
     def firstShot
       if @generator.rand(1.0) < @FIRSTSHOTPROB
-        return GameCharacter::SPACESTATION
+        GameCharacter::SPACESTATION
       else
-        return GameCharacter::ENEMYSTARSHIP
+        GameCharacter::ENEMYSTARSHIP
       end
     end
 
     #Método que determina si la estación espacial se moverá para esquivar un disparo.
     def spaceStationMoves(speed)
-      @generator.rand 1.0 < speed
+      @generator.rand(1.0) < speed
     end
 
   end
