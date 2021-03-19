c5b42270806bf274d4184ce4d4003d7002044bcf
mariogmarq <mariogarciamarq@gmail.com>
2021-03-19 11:00:53 +0100

    Damage y Hangar hecho
    Damage un poco F

---
+++ practicas/practica2/src/main/java/deepspace/Damage.java
@@ -0,0 +1,114 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deepspace;
+
+import java.util.ArrayList;
+
+/**
+ * Representa el daño producido a una estación espacial por una naveenemiga cuando se pierde un combate
+ */
+public class Damage {
+    private int nShields;
+    private int nWeapons;
+    private ArrayList<WeaponType> weapons;
+
+    Damage(int s, int w) {
+        this.nShields = s;
+        this.nWeapons = w;
+        weapons = new ArrayList<>();
+    }
+    
+    Damage(ArrayList<WeaponType> wl, int s) {
+        this.weapons = new ArrayList<>(wl);
+        this.nShields = s;
+        this.nWeapons = weapons.size();
+    }
+    
+    Damage(Damage d) {
+        this(d.weapons, d.nShields);
+    }
+    
+    DamageToUI getUIversion() {
+        return new DamageToUI(this);
+    }
+    
+    /**
+     * @param w array de armas
+     * @param t tipo de arma
+     * @return devuelve el indice del primer elemento de w que sea de tipo t, -1 si no hay ninguno
+     */
+    private int arrayContainsType(ArrayList<Weapon> w, WeaponType t) {
+        int index = -1;
+        for (int i = 0; i < w.size(); i++) {
+            if(w.get(i).getType() == t) {
+                index = i;
+                break;
+            }
+        }
+        
+        return index;
+    }
+    
+    /**
+     * Devuelve una versión ajustada del objeto a las colecciones de armas y potenciadores de escudos suministradas como parámetro.
+     * @param w armas
+     * @param s potenciadores
+     * @return version ajustada del objeto
+     * @deprecated no implementado
+     */
+    public Damage adjust(ArrayList<Weapon> w, ArrayList<ShieldBooster> s) {
+        throw new UnsupportedOperationException();
+    }
+    
+    /**
+     * intenta eliminar el tipo del arma pasada como parámetro de esa lista.
+     * En otro caso simplementedecrementa en una unidad el contador de armas que deben ser eliminadas.
+     * Ese contador no puedeser inferior a cero en ningún caso
+     * @param w el arma
+     */
+    public void discardWeapon(Weapon w) {
+        int index = weapons.indexOf(w.getType());
+        if(index == -1) {
+            nWeapons--;
+            if(nWeapons < 0) {
+                nWeapons = 0;
+            }
+        } else {
+            weapons.remove(index);
+        }
+    }
+    
+    /**
+     * Descarte un shieldBooster
+     */
+    public void discardShieldBooster() {
+        nShields--;
+        if(nShields < 0){
+            nShields = 0;
+        }
+    }
+    
+    /**
+     * @return rue si el daño representado no tiene ningún efecto.
+     * Esto quiere decir que no implica la pérdida de ningún tipo de accesorio (armas o potenciadores de escudo).
+     */
+    public boolean hasNoEffect() {
+        return (nWeapons == 0 && nShields == 0);
+    }
+
+    public ArrayList<WeaponType> getWeapons() {
+        return new ArrayList<>(weapons);
+    }
+
+    public int getNWeapons() {
+        return nWeapons;
+    }
+
+    public int getNShields() {
+        return nShields;
+    }
+    
+}
---
+++ practicas/practica2/src/main/java/deepspace/Hangar.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deepspace;
+
+import java.util.ArrayList;
+
+/**
+ * Representa un hangar
+ */
+public class Hangar {
+    private int maxElements;
+    private ArrayList<Weapon> weapons;
+    private ArrayList<ShieldBooster> shieldBoosters;
+
+    Hangar(int capacity) {
+        this.maxElements = capacity;
+        weapons = new ArrayList<>();
+        shieldBoosters = new ArrayList<>();
+    }
+    
+    Hangar(Hangar h) {
+       this.maxElements = h.getMaxElements();
+       this.weapons = h.getWeapons();
+       this.shieldBoosters = h.getShieldBoosters();
+    }
+    
+   HangarToUI getUIversion() {
+        return new HangarToUI(this);
+    }
+    
+   /**
+    * @return true si aún hay espacio para añadir elementos y que por lotanto no se ha llegado a la capacidad máxima
+    */
+   private boolean spaceAvailable() {
+       return (weapons.size() + shieldBoosters.size()) < maxElements;
+   }
+   /**
+    * Agrega w al hangar
+    * @param w el arma a agregar
+    * @return si se ha podido agregar por el tamanio
+    */
+   public boolean addWeapon(Weapon w) {
+       if (this.spaceAvailable()) {
+           weapons.add(w);
+           return true;
+       }
+       
+       return false;
+   }
+   
+   /**
+    * Agrega s al hangar
+    * @param s el shieldbooster a agregar
+    * @return si se ha podido agregar por el tamanio
+    */
+   public boolean addShieldBooster(ShieldBooster s) {
+       if (this.spaceAvailable()) {
+           shieldBoosters.add(s);
+           return true;
+       }
+       
+       return false;
+   }
+   
+   /**
+    * @return capacidad del hangar
+    */
+    public int getMaxElements() {
+        return maxElements;
+    }
+
+    /**
+     * @return escudos del hangar
+     */
+    public ArrayList<ShieldBooster> getShieldBoosters() {
+        return new ArrayList<>(shieldBoosters);
+    }
+    
+    /**
+     * @return armas del hangar
+     */
+    public ArrayList<Weapon> getWeapons() {
+        return new ArrayList<>(weapons);
+    }
+   
+    /**
+     * Elimina el escudo número s del hangar y la devuelve, siempre que esta exista.
+     * @param s indice del arma
+     * @return  Si el índice suministrado es incorrecto devuelve null, devuelve el escudo
+     */
+    public ShieldBooster removeShieldBooster(int s){
+        if(s >= shieldBoosters.size() || s < 0) {
+             return null;
+         }
+         
+        return shieldBoosters.remove(s);
+    } 
+    
+    /**
+     * Elimina el arma número w del hangar y la devuelve, siempre que esta exista.
+     * @param w indice del arma
+     * @return Si el índice suministrado es incorrecto devuelve null, devuelve el arma
+     */
+    public Weapon removeWeapon(int w){
+         if(w >= weapons.size() || w < 0) {
+             return null;
+         }
+         
+        return weapons.remove(w);
+    }
+
+    @Override
+    public String toString() {
+        return "maxElements: " + maxElements + " weapons: " + weapons.toString() + " shieldBoosters: " + shieldBoosters.toString();
+    }
+    
+    
+    
+}
